d6ebe1d724d33a450f85a74620b527b5b6556f31
Rufin Hounkpe <rhounkpe@gmail.com>
2020-12-01 20:47:29 +0100

    Adding image view

--- app/src/main/java/be/digitcom/diceroller/MainActivity.kt
+++ app/src/main/java/be/digitcom/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package be.digitcom.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +15,22 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's roll"
 
         rollButton.setOnClickListener {
-            // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
